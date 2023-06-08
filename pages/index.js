addd99dbf0064017a68e7533f6cfc4e88e804c1e
RyoLinEth <ryolindev@gmail.com>
2023-06-09 05:47:27 +0800

    Initt

---
+++ pages/index.js
@@ -0,0 +1,38 @@
+import Head from "next/head";
+import Banner from "@/components/Banner/Banner";
+import ContactOne from "@/components/Contact/ContactOne";
+import Roadmap from "@/components/Roadmap/Roadmap";
+import Sales from "@/components/Sales/Sales";
+import TeamOne from "@/components/Team/TeamOne";
+import TopPartners from "@/components/TopPartners/TopPartners";
+import WhitePaper from "@/components/WhitePaper/WhitePaper";
+import WhoWeAre from "@/components/WhoWeAre/WhoWeAre";
+import WhyChooseUs from "@/components/WhyChooseUs/WhyChooseUs";
+import LayoutOne from "@/layouts/LayoutOne";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>BNBTiger - The Greatest MEME in BSC</title>
+        <meta name="description" content="" />
+      </Head>
+
+      <LayoutOne>
+        <main className="fix">
+          <Banner />
+          <WhoWeAre />
+
+          <WhyChooseUs />
+          <Sales />
+
+          <div className="area-bg">
+            <Roadmap />
+          </div>
+
+          <ContactOne />
+        </main>
+      </LayoutOne>
+    </>
+  );
+}
