076e0eb5e826596509d27c86232acf420f07db49
RyoLinEth <ryolindev@gmail.com>
2023-06-09 21:24:01 +0800

    PageLogo

--- pages/_document.js
+++ pages/_document.js
@@ -7,9 +7,9 @@ export default function Document() {
         <meta charset="utf-8" />
         <meta http-equiv="x-ua-compatible" content="ie=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/img/favicon.png" />
-        <link rel="shortcut icon" type="image/x-icon" href="/img/favicon.png" />
-        <link rel="apple-touch-icon" href="/img/favicon.png" />
+        <link rel="icon" href="/img/svg/Logo.png" />
+        <link rel="shortcut icon" type="image/x-icon" href="/img/svg/Logo.png" />
+        <link rel="apple-touch-icon" href="/img/svg/Logo.png" />
       </Head>
 
       <body>
